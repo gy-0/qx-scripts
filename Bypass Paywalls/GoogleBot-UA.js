1240dfcab74afa7fc95b19cab08244acdf7c4986
Gao Yuan <gy001@foxmail.com>
2023-02-18 22:02:09 +0800

    Update GoogleBot-UA.js

--- Bypass Paywalls/GoogleBot-UA.js
+++ Bypass Paywalls/GoogleBot-UA.js
@@ -1,7 +1,7 @@
 ;(async () => {
     const headers = $request.headers
   
-    delete headers['user-agent']
+    delete headers['User-Agent']
     
     headers['User-Agent'] = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
